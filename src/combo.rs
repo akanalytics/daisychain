9169c0014150fadaabdebf79d364500c5e2eaa77
Andy Watkins <andy@webcalculator.com>
2023-05-24 18:46:11 +0100

    M3: root parser

--- src/combo.rs
+++ src/combo.rs
@@ -10,7 +10,7 @@ fn type_suffix(type_name: &str) -> &str {
     }
 }
 
-pub trait ParserT0<'a>: Sized {
+pub trait Parser<'a>: Sized {
     type Input;
     type Error;
 
@@ -21,13 +21,23 @@ pub trait ParserT0<'a>: Sized {
             error = std::any::type_name::<Self::Error>()
         )
     }
+}
+
+pub trait ParserT0<'a>: Parser<'a> {
+    fn name(&self, indent: &str) -> String {
+        format!(
+            "{indent}parser({input}) -> Result<(), {error}>",
+            input = std::any::type_name::<Self::Input>(),
+            error = std::any::type_name::<Self::Error>()
+        )
+    }
     fn parse(&mut self, inp: Self::Input) -> Result<Self::Input, Self::Error>;
-    fn chain_lex<P2: ParserT0<'a>>(self, p2: P2) -> Chain30<'a, Self, P2, (), (), ()>
+    fn chain_lex<P2: ParserT0<'a>>(self, p2: P2) -> Chain<'a, Self, P2, (), (), ()>
     where
         Self: ParserT0<'a>,
         P2: ParserT0<'a, Input = Self::Input, Error = Self::Error>,
     {
-        Chain30 {
+        Chain {
             p1: self,
             p2,
             pdlt: Default::default(),
@@ -51,12 +61,12 @@ pub trait ParserT1<'a, T0>: Sized {
         )
     }
     fn parse(&mut self, inp: Self::Input) -> Result<(Self::Input, T0), Self::Error>;
-    fn chain_lex2<P2: ParserT0<'a>>(self, p2: P2) -> Chain30<'a, Self, P2, T0, (), ()>
+    fn chain_lex2<P2: ParserT0<'a>>(self, p2: P2) -> Chain<'a, Self, P2, T0, (), ()>
     where
         //     // Self: ParserT0<'a>,
         P2: ParserT0<'a, Input = Self::Input, Error = Self::Error>,
     {
-        Chain30 {
+        Chain {
             p1: self,
             p2,
             pdlt: Default::default(),
@@ -116,12 +126,13 @@ pub trait ParserT2<'a, T0, T1> {
 //     }
 // }
 
-impl<'a, F> ParserT0<'a> for F
+impl<'a, F> Parser<'a> for F
 where
     F: FnMut(&'a str) -> Result<&'a str, ParseError>,
 {
     type Error = ParseError;
     type Input = &'a str;
+
     fn name(&self, indent: &str) -> String {
         format!(
             "{indent}ParserT0 {func}({input}) -> Result<({input}), {error}>",
@@ -130,6 +141,14 @@ where
             error = type_suffix(std::any::type_name::<Self::Error>())
         )
     }
+}
+
+impl<'a, F> ParserT0<'a> for F
+where
+    F: FnMut(&'a str) -> Result<&'a str, ParseError>,
+{
+    // type Error = ParseError;
+    // type Input = &'a str;
     fn parse(&mut self, s: &'a str) -> Result<&'a str, ParseError> {
         // trace!("#### FnMut(SelectableStr): {s}", s = s.cur.unwrap_or("-"));
         (self)(s)
@@ -167,10 +186,10 @@ where
 //     pd: PhantomData<&'a ()>,
 // }
 
-impl<'a, P1, P2> ParserT0<'a> for Chain30<'a, P1, P2, (), (), ()>
+impl<'a, P1, P2> Parser<'a> for Chain<'a, P1, P2, (), (), ()>
 where
-    P1: ParserT0<'a>,
-    P2: ParserT0<'a, Input = P1::Input, Error = P1::Error>,
+    P1: Parser<'a>,
+    P2: Parser<'a, Input = P1::Input, Error = P1::Error>,
 {
     type Input = P1::Input;
     type Error = P1::Error;
@@ -184,6 +203,13 @@ where
             t = self.p2.name(&format!("{indent}  ")),
         )
     }
+}
+
+impl<'a, P1, P2> ParserT0<'a> for Chain<'a, P1, P2, (), (), ()>
+where
+    P1: ParserT0<'a>,
+    P2: ParserT0<'a, Input = P1::Input, Error = P1::Error>,
+{
     fn parse(&mut self, inp: Self::Input) -> Result<Self::Input, Self::Error> {
         let i1 = self.p1.parse(inp)?;
         match self.p2.parse(i1) {
@@ -193,7 +219,7 @@ where
     }
 }
 
-pub struct Chain30<'a, S, T, S0, S1, S2>
+pub struct Chain<'a, S, T, S0, S1, S2>
 // where
 //     S: ParserT1<'a, S0>,
 //     T: ParserT0<'a, Input = S::Input, Error = S::Error>,
@@ -206,14 +232,11 @@ pub struct Chain30<'a, S, T, S0, S1, S2>
     pd2: PhantomData<S2>,
 }
 
-impl<'a, S, T, S0> ParserT1<'a, S0> for Chain30<'a, S, T, S0, (), ()>
+impl<'a, S, T, S0, S1, S2> Chain<'a, S, T, S0, S1, S2>
 where
-    S: ParserT1<'a, S0>,
-    T: ParserT0<'a, Input = S::Input, Error = S::Error>,
+    S: Parser<'a>,
+    T: Parser<'a, Input = S::Input, Error = S::Error>,
 {
-    type Input = S::Input;
-    type Error = S::Error;
-
     fn name(&self, indent: &str) -> String {
         format!(
             "{indent}chain10\n{s}\n{t}",
@@ -223,6 +246,15 @@ where
             t = self.p2.name(&format!("{indent}  ")),
         )
     }
+}
+
+impl<'a, S, T, S0> ParserT1<'a, S0> for Chain<'a, S, T, S0, (), ()>
+where
+    S: ParserT1<'a, S0>,
+    T: ParserT0<'a, Input = S::Input, Error = S::Error>,
+{
+    type Input = S::Input;
+    type Error = S::Error;
 
     fn parse(&mut self, inp: Self::Input) -> Result<(Self::Input, S0), Self::Error> {
         let (i1, s0) = self.p1.parse(inp)?;
@@ -244,7 +276,7 @@ impl SP {
     }
 }
 
-impl<'a> ParserT0<'a> for SP {
+impl<'a> Parser<'a> for SP {
     type Input = &'a str;
     type Error = ParseError;
     fn name(&self, indent: &str) -> String {
@@ -254,7 +286,9 @@ impl<'a> ParserT0<'a> for SP {
             error = std::any::type_name::<Self::Error>()
         )
     }
+}
 
+impl<'a> ParserT0<'a> for SP {
     fn parse(&mut self, inp: Self::Input) -> Result<Self::Input, Self::Error> {
         Ok(inp)
     }
@@ -275,6 +309,11 @@ mod tests {
             Ok(&s[1..])
         }
 
+        fn ws<'a>(s: &'a str) -> Result<&'a str, ParseError> {
+            Ok(s.trim_start())
+        }
+
+
         fn num_parser<'a>(s: &'a str) -> Result<(&'a str, i32), ParseError> {
             Ok((&s[1..], s[0..=0].parse::<i32>()?))
         }
@@ -283,15 +322,15 @@ mod tests {
 
         let mut parser1 = SP
             .chain_lex(tail_parser)
-            .chain_lex(tail_parser)
+            .chain_lex(ws)
             .chain_lex(tail_parser);
 
         let parser2 = SP
             .chain_lex(|s| tail_parser(s))
-            .chain_lex(tail_parser)
+            .chain_lex(ws)
             .chain_lex(tail_parser);
 
-        let s = parser1.parse("cat2dog").unwrap();
+        let s = parser1.parse("c   t2dog").unwrap();
         assert_eq!(s, "2dog");
         println!("{}", parser1.name(""));
         println!("{}", parser2.name(""));
@@ -304,7 +343,7 @@ mod tests {
         assert_eq!(d, 5);
 
         let mut parser4 = parser3.chain_lex2(parser1);
-        let (s, d) = parser4.parse("6dogs").unwrap();
+        let (s, d) = parser4.parse("6d   gs").unwrap();
         assert_eq!(s, "s");
         assert_eq!(d, 6);
         println!("{}", parser4.name(""));
